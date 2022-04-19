aba72e490dbf546a2a13aeeecd310f0590e2219f
Otakus3nnin <otakus3nnin@gmail.com>
2022-04-19 23:43:42 +0100

    Dark mode complete

--- front-end_mentor/social-media-dashboard/app.js
+++ front-end_mentor/social-media-dashboard/app.js
@@ -1,6 +1,14 @@
 let inner =  document.querySelector('.switch-inner');
 let outer = document.querySelector('.switch-outer');
-inner.addEventListener('click',function(e){
-    e.target.classList.toggle('dark');
-    outer.classList.toggle('dark');
+let card = document.querySelectorAll('.card');
+let body = document.querySelector('body');
+let title = document.querySelector('.title');
+outer.addEventListener('click',function(e){
+    e.currentTarget.classList.toggle('dark');
+    inner.classList.toggle('dark');
+    body.classList.toggle('dark');
+    card.forEach(item =>{
+        item.classList.toggle('dark');
+    })
+    title.classList.toggle('dark');
 })
